d8faf05d97dd10def63eb9b697a45e9c209ceb22
MoMo LaFrOOO <62650527+momolafrooo@users.noreply.github.com>
2022-02-22 10:23:01 +0000

    fix github repo api

---
+++ src/app/services/api/GithubRepoApi/index.ts
@@ -0,0 +1,49 @@
+import { rootApi } from '../';
+
+import { GithubRepo } from './types';
+
+export const GithubRepoApi = rootApi.injectEndpoints({
+  endpoints: builder => ({
+    getAllGithubRepo: builder.query<GithubRepo[], void>({
+      query: () => `/githubrepo`,
+    }),
+
+    getGithubRepoById: builder.query<GithubRepo, number>({
+      query: (id: number) => `/githubrepo/${id}`,
+    }),
+
+    saveGithubRepo: builder.mutation<GithubRepo, GithubRepo>({
+      query: (payload: GithubRepo) => ({
+        url: `/githubrepo`,
+        method: 'POST',
+        body: payload,
+      }),
+    }),
+
+    updateGithubRepo: builder.mutation<GithubRepo, GithubRepo>({
+      query: (payload: GithubRepo) => ({
+        url: `/GithubRepo`,
+        method: 'PUT',
+        body: payload,
+      }),
+    }),
+
+    deleteGithubRepo: builder.mutation<GithubRepo, number>({
+      query: (id: number) => ({
+        url: `/githubrepo/${id}`,
+        method: 'DELETE',
+      }),
+    }),
+  }),
+  overrideExisting: false,
+});
+
+// Export hooks for usage in function components, which are
+// auto-generated based on the defined endpoints
+export const {
+  useGetGithubRepoByIdQuery,
+  useGetAllGithubRepoQuery,
+  useSaveGithubRepoMutation,
+  useUpdateGithubRepoMutation,
+  useDeleteGithubRepoMutation,
+} = GithubRepoApi;
